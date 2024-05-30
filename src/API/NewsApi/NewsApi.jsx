e9304837829b7aa518f81d7138b44dcca5ef0dec
RickyAnchores <ricky23anchores@gmail.com>
2024-05-30 09:55:06 +0200

    updated news api

--- src/API/NewsApi/NewsApi.jsx
+++ src/API/NewsApi/NewsApi.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState,useEffect} from 'react'
 
 const NewsApi = () => {
 
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error,setError] = useState(null);
 
     const fetchData = async () => {
-        let url = ""
-        let myApiKey = "097e9acec10d424c94334295d2747fd8"
+      const myApiKey = "097e9acec10d424c94334295d2747fd8";
+      const url = `https://newsapi.org/v2/everything?q=keyword&apiKey=${myApiKey}`;
         try{
-            
+            let res = await axios.get(url)
+            setArticles(res.data.articles)
+            setLoading(false)
+            console.log(res)
         }catch(err){
-            console.log("Something went wrong",err)
+            setError("Something went wrong",err)
+            setLoading(false)
         }
-    }
+    };
+
+    useEffect(() => {
+      fetchData()
+    }, [])
+    
+
+    if(loading) return <p>Loading. . .</p>
+    if(error) return <p>Something went wrong: {error}</p>
 
 
   return (
-    <div>NewsApi</div>
+    <div className='News'>
+      <h1>News API</h1>
+      <ul>
+        {articles.map((article,index) => (
+          <li key={index}>
+            <img src={article.urlToImage} alt="image" />
+            <a href={article.url} target='_blank'>
+              {article.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import NewsApi from '../../API/NewsApi/NewsApi';
 
 const Home = () => {
   return (
     <div className='Home'>
         <h1>R NEWS</h1>
         <p>Test News API</p>
+        <div className="container">
+          <NewsApi />
+        </div>
     </div>
   )
 }
